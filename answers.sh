1b862c2c7b11faff408ca9c21dbaa2605abaf8ef
Lydian Bridget Li <lbli@data.cs.purdue.edu>
2021-09-05 16:42:22 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,40 +4,43 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv fafsa.jpg Memes/SpongeBob
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
-
+cp Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/Python3
 
 # Task 10
-
+touch Homework/POl141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
-: '
+:set number
+:set showmatch
+:syntax on
+' 
 Type your vim/nano customizations below:
 
 
